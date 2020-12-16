0731b5479042097ee558f81c3d4a4c3df6100973
panpanh5 <974485374@qq.com>
2020-12-16 21:10:45 +0800

    添加主页面-头部组件-底部组件

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <van-tabbar active="{{ active }}" v-bind:change="onChange">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+    };
+  },
+  onChange(event) {
+    // event.detail 的值为当前选中项的索引
+    this.setData({ active: event.detail });
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/admin/admin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <!-- 头部 -->
+      <!-- 中间显示 -->
+     
+      <router-view></router-view>
+      <!-- 底部固定 -->
+      <Footer></Footer>
+      
+  </div>
+</template>
+
+<script>
+import footer from'../../components/footer/footer'
+export default {
+    components:{
+        'Footer':footer}
+};
+</script>
+
+<style>
+</style>
