d022028be1407e056be43c16c2cbf76823ea3f5e
pan <974485374@qq.com>
2020-12-17 14:35:20 +0800

    修改打卡

--- src/views/location.vue
+++ src/views/location.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="location">
-    打卡利用高德Api获取位置信息
+    <div>
+      <div class="header">
+        当前打卡位置:
+        <div>
+          weizhi
+        </div>
+      </div>
+      <div class="container">星期{{ currentTime.week }} 打卡</div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      // 可以自定义管道操作符来处理
+      currentTime: {
+        week: new Date().getDay(),
+        time: "",
+        month: new Date().getMonth() + 1,
+      },
+    };
+  },
+  methods: {
+    getTime() {
+      console.log(new Date());
+    },
+  },
+};
 </script>
 
 <style></style>
